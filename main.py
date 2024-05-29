75e71e44ae7d6eb3e64bd434f64460dd06a758d3
canali000 <161646434+canali000@users.noreply.github.com>
2024-05-29 19:41:17 +0300

    patch-1

--- main.py
+++ main.py
@@ -1,33 +1,303 @@
-import string #string kütüphanesi metin analizi için çeşitli araçlar sağlar. Biz kodumuzda noktalama işaretlerini kaldırmak için kullandık
-from collections import Counter #Python'ın collections kütüphanesinde bulunan Counter sınıfı verileri sayma işlemlerini kolaylaştırır
+from operation import *
+import tkinter as tk
+from tkinter import filedialog
+from tkinter import messagebox
+import sqlite3
 
 
-class Metin:
-    def __init__(self, dosya_yolu):
-        self.dosya_yolu = dosya_yolu # Dosya yolunu saklar
-        self.icerik = self.dosya_okuma() # Dosya içeriğini okur
-        self.kelimeler = self.metin_isleme() # Metni işler ve kelimelere ayırır
+# Button Action
+def load_file1():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path:
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read()
+            textPlace1.delete(1.0, tk.END)
+            textPlace1.insert(tk.END, content)
 
-    def dosya_okuma(self): # Dosya okur
-        with open(self.dosya_yolu, 'r', encoding='utf-8') as file:
-            return file.read()
+def load_file2():
+    file_path = filedialog.askopenfilename(filetypes=[("Text files", "*.txt")])
+    if file_path:
+        with open(file_path, "r", encoding="utf-8") as file:
+            content = file.read()
+            textPlace2.delete(1.0, tk.END)
+            textPlace2.insert(tk.END, content)
 
-    def metin_isleme(self):
-        temiz_icerik = self.icerik.translate(str.maketrans('', '', string.punctuation)) # Noktalama işaretlerini kaldırır
-        kelimeler = temiz_icerik.lower().split() # Metni küçük harfe dönüştürür ve kelimelere ayırır
-        return kelimeler
 
-    def harf_sayisi(self):# Toplam harf sayısını hesaplar
-        return sum(len(kelime) for kelime in self.kelimeler)
+def highlight_text(text_widget, keyword):
+    text_widget.tag_remove("highlight", 1.0, tk.END)
+    if keyword:
+        start = "1.0"
+        while True:
+            start = text_widget.search(keyword, start, stopindex=tk.END, nocase = True)
+            if not start:
+                break
+            end = f"{start}+{len(keyword)}c"
+            text_widget.tag_add("highlight", start, end)
+            start = end
+        text_widget.tag_config("highlight", background="yellow")
 
-    def kelime_sayisi(self): #Toplam kelime sayısını hesaplar
-        return len(self.kelimeler)
+def search_and_highlight():
+    keyword = search_entry.get()
+    highlight_text(textPlace1, keyword)
+    highlight_text(textPlace2, keyword)
 
-    def etkisiz_kelimeler(self, etkisiz_kelimeler_listesi): # Etkisiz kelimeleri listeden çıkarır ve kalan kelimeleri döndürür
-        return [kelime for kelime in self.kelimeler if kelime not in etkisiz_kelimeler_listesi]
 
-    def kelime_istatistikleri(self):
-        kelime_sayaci = Counter(self.kelimeler) #self kelimeler listesindeki her bir kelimenin kaç kez geçtiğini sayar
-        en_cok_gecen = kelime_sayaci.most_common(5) # En yaygın kullanılan 5 kelimeyi bulur
-        en_az_gecen = kelime_sayaci.most_common()[:-6:-1] # En az kullanılan 5 kelimeyi bulur
-        return en_cok_gecen, en_az_gecen
+def compare_texts():
+    text1_content = textPlace1.get(1.0, tk.END).strip()
+    text2_content = textPlace2.get(1.0, tk.END).strip()
+    if not text1_content or not text2_content:
+        messagebox.showwarning("Uyarı!", "Her iki metin kutusu da dolu olmak zorundadır.")
+        return None
+
+    similarity_ratio = Similarity().percentage(text1_content, text2_content)
+    result_label.config(text=f"Benzerlik Yüzdesi: %{similarity_ratio}")
+
+def show_stats1():
+    text1_content = textPlace1.get(1.0, tk.END).strip()
+    if not text1_content:
+        messagebox.showwarning("Uyarı!", "1. Metin kutusu dolu olmak zorunda.")
+        return None
+
+    wcount_label.config(text=f"Kelime Sayısı: {TextData(text1_content).get_wordCount()}")
+    lcount_label.config(text=f"Harf Sayısı: {TextData(text1_content).get_letterCount()}")
+    useless_label.config(text=f"Etkisiz Kelime Sayısı: {TextData(text1_content).get_uselessWordCount()}")
+    maxWord.config(text=f"En Çok Geçen Kelimeler: {TextData(text1_content).get_maxWord()}")
+    minWord.config(text=f"En Az Geçen Kelimeler: {TextData(text1_content).get_minWord()}")
+
+
+def show_stats2():
+    text2_content = textPlace2.get(1.0, tk.END).strip()
+    if not text2_content:
+        messagebox.showwarning("Uyarı!", "2. Metin kutusu dolu olmak zorunda.")
+        return None
+
+    wcount_label2.config(text=f"Kelime Sayısı: {TextData(text2_content).get_wordCount()}")
+    lcount_label2.config(text=f"Harf Sayısı: {TextData(text2_content).get_letterCount()}")
+    useless_label2.config(text=f"Etkisiz Kelime Sayısı: {TextData(text2_content).get_uselessWordCount()}")
+    maxWord2.config(text=f"En Çok Geçen Kelimeler: {TextData(text2_content).get_maxWord()}")
+    minWord2.config(text=f"En Az Geçen Kelimeler: {TextData(text2_content).get_minWord()}")
+
+def upload_db():
+    text1_content = textPlace1.get(1.0, tk.END).strip()
+    text2_content = textPlace2.get(1.0, tk.END).strip()
+    if not text1_content or not text2_content:
+        messagebox.showwarning("Uyarı!", "Her iki metin kutusu da dolu olmak zorundadır.")
+        return None
+
+    wcount_data1 = f"Kelime Sayısı: {TextData(text1_content).get_wordCount()}"
+    lcount_data1 = f"Harf Sayısı: {TextData(text1_content).get_letterCount()}"
+    useless_data1 = f"Etkisiz Kelime Sayısı: {TextData(text1_content).get_uselessWordCount()}"
+    maxWord_data1 = f"En Çok Geçen Kelimeler: {TextData(text1_content).get_maxWord()}"
+    minWord_data1 = f"En Az Geçen Kelimeler: {TextData(text1_content).get_minWord()}"
+
+    wcount_data2 = f"Kelime Sayısı: {TextData(text2_content).get_wordCount()}"
+    lcount_data2 = f"Harf Sayısı: {TextData(text2_content).get_letterCount()}"
+    useless_data2 = f"Etkisiz Kelime Sayısı: {TextData(text2_content).get_uselessWordCount()}"
+    maxWord_data2 = f"En Çok Geçen Kelimeler: {TextData(text2_content).get_maxWord()}"
+    minWord_data2 = f"En Az Geçen Kelimeler: {TextData(text2_content).get_minWord()}"
+
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+        # Multi Data
+        data = [
+            (text1_content, wcount_data1, lcount_data1, useless_data1, maxWord_data1, minWord_data1),
+            (text2_content, wcount_data2, lcount_data2, useless_data2, maxWord_data2, minWord_data2)
+        ]
+        
+        cursor.executemany('''
+        INSERT INTO stats (text, wcount, lcount, useless, max, min)
+        VALUES (?, ?, ?, ?, ?, ?)
+        ''', data)
+
+        conn.commit()
+
+    messagebox.showinfo("", "Başarıyla Yüklendi.")
+
+def clear_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+
+    cursor.execute("DELETE FROM stats")
+
+    conn.commit()
+
+    messagebox.showinfo("", "Başarıyla Temizlendi.")
+
+def fetch_db():
+    with sqlite3.connect("database.db") as conn:
+        cursor = conn.cursor()
+
+        id = int(spinbox.get())
+            
+        cursor.execute("SELECT * FROM stats WHERE id = ?", (id,))
+        data = cursor.fetchone()
+        if data:
+            content = data[1]
+            wcount = data[2]
+            lcount = data[3]
+            useless = data[4]
+            maxWord = data[5]
+            minWord = data[6]
+
+            dataTextPlace.delete(1.0, tk.END)
+            dataTextPlace.insert(tk.END, content)
+            wcount_label3.config(text=wcount)
+            lcount_label3.config(text=lcount)
+            useless_label3.config(text=useless)
+            maxWord3.config(text=maxWord)
+            minWord3.config(text=minWord)
+
+        else:
+            dataTextPlace.delete(1.0, tk.END)
+            dataTextPlace.insert(tk.END, "Veri Bulunamadı")
+            wcount_label3.config(text="")
+            lcount_label3.config(text="")
+            useless_label3.config(text="")
+            maxWord3.config(text="")
+            minWord3.config(text="")
+
+with sqlite3.connect("database.db") as conn:
+
+    cursor = conn.cursor()
+
+    cursor.execute("""
+        CREATE TABLE IF NOT EXISTS stats
+        (id INTEGER PRIMARY KEY, 
+        text TEXT, 
+        wcount TEXT, 
+        lcount TEXT,
+        useless TEXT,
+        max TEXT,
+        min TEXT)
+    """)
+
+    conn.commit()
+
+
+# Window
+window = tk.Tk()
+window.title("NLP ile Metin Analizi")
+window.minsize(width=1300, height=700)
+
+
+# Label
+label = tk.Label(text="Default")
+label.config(text="1. Metin")
+label.grid(column=0, row=0)
+
+# Text
+textPlace1 = tk.Text(height=10, width=30, font="Arial")
+textPlace1.focus()
+textPlace1.insert(tk.END, "")
+print(textPlace1.get("1.0", tk.END))
+textPlace1.grid(column=0, row=1, padx=20)
+
+# Load
+load_button = tk.Button(window, text="Dosya Yükle", command=load_file1)
+load_button.grid(column=0, row=2)
+
+# Search
+search_label = tk.Label(window, text="Kelime Arama")
+search_label.grid(row=2, column=1)
+search_entry = tk.Entry(window)
+search_entry.grid(row=3, column=1)
+search_button = tk.Button(window, text="Ara ve Vurgula", command=search_and_highlight)
+search_button.grid(row=4, column=1)
+
+
+# Label2
+label = tk.Label(text="Default")
+label.config(text="2. Metin")
+label.grid(column=2, row=0)
+
+# Text
+textPlace2 = tk.Text(height=10, width=30, font="Arial")
+textPlace2.insert(tk.END, "")
+print(textPlace2.get("1.0", tk.END))
+textPlace2.grid(column=2, row=1,padx=20)
+
+# Load
+load_button = tk.Button(window, text="Dosya Yükle", command=load_file2)
+load_button.grid(column=2, row=2)
+
+# Stats
+stats_button = tk.Button(window, text="İstatistikleri Göster", command=show_stats1)
+stats_button.grid(row=3, column=0)
+
+wcount_label = tk.Label(window)
+wcount_label.grid(row=4, column=0)
+lcount_label = tk.Label(window)
+lcount_label.grid(row=5, column=0)
+useless_label = tk.Label(window)
+useless_label.grid(row=6, column=0)
+maxWord = tk.Label(window, wraplength=200)
+maxWord.grid(row=7, column=0)
+minWord = tk.Label(window, wraplength=200)
+minWord.grid(row=10, column=0)
+
+# Stats 2
+wcount_label2 = tk.Label(window)
+wcount_label2.grid(row=4, column=2)
+lcount_label2 = tk.Label(window)
+lcount_label2.grid(row=5, column=2)
+useless_label2 = tk.Label(window)
+useless_label2.grid(row=6, column=2)
+maxWord2 = tk.Label(window, wraplength=200)
+maxWord2.grid(row=7, column=2)
+minWord2 = tk.Label(window, wraplength=200)
+minWord2.grid(row=10, column=2)
+
+stats_button2 = tk.Button(window, text="İstatistikleri Göster", command=show_stats2)
+stats_button2.grid(row=3, column=2)
+
+
+# Similarity
+compare_button = tk.Button(window, text="Benzerliği Karşılaştır", command=compare_texts)
+compare_button.grid(row=5, column=1)
+result_label = tk.Label(window, text="Benzerlik Yüzdesi: ")
+result_label.grid(row=6, column=1)
+
+# Send Database
+dbsend = tk.Button(window, text="Veritabanına Yükle", command=upload_db)
+dbsend.grid(row=1, column=1, pady=0)
+
+
+# Fetch Spinbox
+def spinbox_used():
+    id = spinbox.get()
+    return id
+
+
+spinbox = tk.Spinbox(from_=1, to=20, width=5, command=None)
+spinbox.grid(row=0,column=4,pady=20)
+
+# Fetch Text
+dataTextPlace = tk.Text(height=10, width=30, font="Arial")
+dataTextPlace.focus()
+dataTextPlace.insert(tk.END, "")
+print(dataTextPlace.get("1.0", tk.END))
+dataTextPlace.grid(column=4, row=1,padx= 20)
+
+# Stats 3
+wcount_label3 = tk.Label(window)
+wcount_label3.grid(row=3, column=4)
+lcount_label3 = tk.Label(window)
+lcount_label3.grid(row=4, column=4)
+useless_label3 = tk.Label(window)
+useless_label3.grid(row=5, column=4)
+maxWord3 = tk.Label(window, wraplength=200)
+maxWord3.grid(row=6, column=4)
+minWord3 = tk.Label(window, wraplength=200)
+minWord3.grid(row=7, column=4)
+
+# Fetch Database
+fetch_data = tk.Button(window, text="Veritabanından Çek", command=fetch_db)
+fetch_data.grid(row=2, column=4)
+
+# Clear Database
+clear_data = tk.Button(window, text="Veritabanını Temizle", command=clear_db)
+clear_data.grid(row=1, column=3)
+
+window.mainloop()
+
+
---
+++ operation.py
@@ -0,0 +1,179 @@
+import re
+from difflib import SequenceMatcher
+
+class Token():
+
+    def __init__(self, data):
+        self.text = data
+
+    def removePunct(self):
+        text = self.text
+
+        turkish_punctuation = [
+            '.', ',', ';', ':', '!', '?', '(', ')',
+            '[', ']', '{', '}', '-', '—', '…', '‘',
+            '’', '“', '”', '«', '»', '/', '\\', '|',
+            '§', '$', '%', '@', '&', '*', '•', '_',
+            '~', '`', '"', "'"
+        ]
+
+        for isaret in turkish_punctuation:
+            text = text.replace(isaret, "")  
+
+        return text.lower()
+
+    def sentenceSplit(self):
+        sentences = re.split(r'(?<!\w\.\w.)(?<![A-Z][a-z]\.)(?<=\.|\?|!)\s', self.text)
+        return sentences
+
+    def wordSplit(self):
+        words = self.removePunct().split()
+        return words
+
+class Counter():
+
+    def __init__(self, words):
+        self.words = words
+
+    def letterCount(self):
+        words = self.words
+        concat = "".join(words)
+        
+        return len(concat)
+    
+    def wordCount(self):
+        words = self.words
+        return len(words)
+
+class Analyzer():
+    def __init__(self, words):
+        self.words = words
+
+    def frequencyMatch(self):
+        words = self.words
+
+        frequency = {}
+        for word in words:
+            if word in frequency:
+                continue          
+
+            count = words.count(word)
+            frequency[word] = count                 
+
+        return frequency
+
+    def wordRarity(self):
+        frequency = self.frequencyMatch()
+        maxFrequency = max(frequency.values())
+        minFrequency = min(frequency.values())
+        maxWords = [word for word in frequency if frequency[word] == maxFrequency]
+        minWords = [word for word in frequency if frequency[word] == minFrequency]
+        
+        if len(maxWords) > 5:
+            maxWords = [maxWords[i] for i in range(5)]
+        if len(minWords) > 5:
+            minWords = [minWords[i] for i in range(5)]
+
+        if maxWords == minWords:
+            return maxWords, ["---"]
+        
+        return maxWords, minWords
+        
+    def uselessWord(self):
+        words = self.words
+
+        etkisiz_kelimeler = [
+            "acaba", "ama", "ancak", "aslında", "az", "bana", "bazı", "belki", "bile", 
+            "bir", "biraz", "birçok", "biri", "birisi", "birkaç", "birşey", "biz", 
+            "bu", "bunu", "bunun", "da", "daha", "de", "defa", "diğer", "diye", 
+            "dışında", "eğer", "en", "gibi", "hem", "hep", "hepsi", "her", "herhangi", 
+            "herkes", "hiç", "hiçbir", "için", "ise", "işte", "kaç", "kadar", 
+            "karşın", "kendi", "kez", "ki", "kim", "kimse", "kise", "mı", "mi", 
+            "mu", "mü", "nasıl", "ne", "neden", "nedenle", "nerde", "nerede", 
+            "nereye", "niçin", "niye", "o", "sadece", "sanki", "şey", "siz", "şu", 
+            "şuna", "şunlar", "şunu", "tarafından", "ve", "veya", "ya", "yani", 
+            "yine", "yoksa", "zaten"
+        ]
+
+        count = 0
+        for useless in etkisiz_kelimeler:
+            if useless in words:
+                times = words.count(useless)
+                count += times
+
+        return count
+
+
+class FileHandler():
+    def __init__(self, path):
+        self.path = path
+        self.data = self.dataCollect()
+        
+
+    def dataCollect(self):
+        path = self.path
+        try:
+            with open(path, mode="r", encoding="utf-8") as readFile:
+                data = readFile.read()
+                return data
+            
+        except FileNotFoundError:
+            with open(path, mode="a", encoding="utf-8") as createFile:
+                data = "Merhaba! Bugün nasılsın? Ben iyiyim. Yarın ne yapacaksın?\n"
+                createFile.write(data)
+                return data
+
+    def upload(self):
+        pass
+
+class TextData:
+    def __init__(self,data):
+        self.text = data
+        self.token = Token(self.text) 
+        self.count = Counter(self.token.wordSplit())
+        self.analyst = Analyzer(self.token.wordSplit())
+
+        self.wordCount = self.count.wordCount()
+        self.letterCount = self.count.letterCount()
+
+        self.uselessWordCount = self.analyst.uselessWord()
+        self.maxWord, self.minWord = self.analyst.wordRarity()
+
+    def get_text(self):
+        return self.text
+
+    def get_wordCount(self):
+        return self.wordCount
+
+    def get_letterCount(self):
+        return self.letterCount
+
+    def get_uselessWordCount(self):
+        return self.uselessWordCount
+
+    def get_maxWord(self):
+        return ", ".join(self.maxWord)
+
+    def get_minWord(self):
+        return ", ".join(self.minWord)
+
+class Similarity:
+    def __init__(self):
+        pass
+
+    def percentage(self, text1, text2):
+        similar = SequenceMatcher(None, text1, text2).ratio()
+        yuzdelik = round(similar * 100, 2)
+        return yuzdelik
+
+    def test(self):
+        text1 = TextData(FileHandler("text1.txt").data).text
+        text2 = TextData(FileHandler("text2.txt").data).text
+        print(text1)
+        print()
+        print(text2)
+        print(Similarity().percentage(text1,text2))
+
+
+# Similarity().test()
+
